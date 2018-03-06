732e58f96c30fd70b8bcb2795f960ee7bf806e66
cuicc <cuchch@hotmail.com>
2018-03-06 16:25:52 +0800

    update

--- CCLoopCollectionView/CCLoopCollectionView.swift
+++ CCLoopCollectionView/CCLoopCollectionView.swift
@@ -77,7 +77,7 @@ public class CCLoopCollectionView: UIView, UICollectionViewDataSource, UICollect
     }
     */
     
-    override init(frame: CGRect) {
+    override public init(frame: CGRect) {
         super.init(frame: frame)
         
         currentFrame = frame
